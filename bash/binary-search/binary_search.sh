f36cd574fd6c2732383f069dba19edf8a2dc97b3
Nathan Withers <nathan@thewithz.tech>
2020-12-23 11:53:29 -0500

    adds solution for binary search

--- bash/binary-search/binary_search.sh
+++ bash/binary-search/binary_search.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+main () {
+    arr=("${@:2}")
+    left=0
+    (( right = ${#arr[@]}-1 ))
+    while (( left <= right )); do
+        (( mid = (left+right)/2 ))
+        (( "$1" == "${arr[$mid]}" )) && echo "$mid" && exit
+        (( "$1" > "${arr[$mid]}" )) && (( left = mid+1 ))
+        (( "$1" < "${arr[$mid]}" )) && (( right = mid-1 ))
+    done
+    echo "-1"
+}
+
+main "$@"
