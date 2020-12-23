16d3c45437d2a76a16a19b682fd0fa43bf291c64
Nathan Withers <nathan@thewithz.tech>
2020-12-23 10:24:07 -0500

    refactors error function to remove magic numbers

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -2,9 +2,9 @@
 
 main () {
     # if there are more or less than 2 arguments, error
-    (( $# != 2 )) && error 1
+    (( $# != 2 )) && error "Usage: hamming.sh <string1> <string2>"
     # both arguments must be the same length
-    (( ${#1} != ${#2} )) && error 2
+    (( ${#1} != ${#2} )) && error "left and right strands must be of equal length"
     count=0
     for ((i=0; i<${#1}; i++)); do 
         [[ "${1:i:1}" != "${2:i:1}" ]] && (( ++count ))
@@ -13,8 +13,7 @@ main () {
 }
 
 error() {
-    (( $1 == 1 )) && echo "Usage: hamming.sh <string1> <string2>"
-    (( $1 == 2 )) && echo "left and right strands must be of equal length"
+    echo "$1"
     exit 1
 }
 
