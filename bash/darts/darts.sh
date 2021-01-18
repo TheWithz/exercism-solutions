8b3dcb53ea12a4fb1516c1a9cb351d163227b118
Nathan Withers <nathan@thewithz.tech>
2021-01-18 17:28:49 -0500

    darts solution

--- bash/darts/darts.sh
+++ bash/darts/darts.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+main () {
+    (( $# == 0 )) && error "invalid args: no args"
+    (( $# == 1 )) && error "invalid args: only 1 arg"
+    # matches floating point values
+    re="^[-]?[0-9]+([.][0-9]+)?$"
+    [[ ! "$1" =~ $re ]] && error "invalid args: first arg non-numeric"
+    [[ ! "$2" =~ $re ]] && error "invalid args: second arg non-numeric"
+    dist=$(bc -l <<< "var1 = $1; var2 = $2; sqrt(var1^2 + var2^2)")
+    score=$(bc << EOF
+    var1 = $dist;
+    if (var1 <= 1) 10 else {
+        if (var1 <= 5) 5 else {
+            if (var1 <= 10) 1 else 0
+        }
+    }
+EOF
+)
+    echo "$score"
+}
+
+error () {
+    echo "$1"
+    exit 1
+}
+
+main "$@"
