9d164b9199b326db0f7e981e4216703563369f17
Nathan Withers <nathan@thewithz.tech>
2021-01-18 19:57:58 -0500

    fixes formatting and shellcheck warning 2018-2019

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -3,14 +3,14 @@
 main () {
     # separates input, one word per line
     tr -s " -" "\n" <<< "$1" |
-        # removes all special characters and numbers, but not newlines
-        tr -cd '[:alpha:]\n' |
-        # changes all letter to uppercase
-        tr a-z A-Z |
-        # grab the first letter of each word
-        cut -c 1 |
-        # stick them back together
-        paste -s -d ''
+    # removes all special characters and numbers, but not newlines
+    tr -cd '[:alpha:]\n' |
+    # changes all letter to uppercase
+    tr [:lower:] [:upper:] |
+    # grab the first letter of each word
+    cut -c 1 |
+    # stick them back together
+    paste -s -d ''
 }
 
 main "$@"
