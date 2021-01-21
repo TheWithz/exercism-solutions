5c81ce6b22963f46aae0ee7d82dd05a887ad3600
Nathan Withers <nathan@thewithz.tech>
2021-01-21 13:10:48 -0500

    adds solution for run length encoding

--- bash/run-length-encoding/run_length_encoding.sh
+++ bash/run-length-encoding/run_length_encoding.sh
@@ -1,24 +1,56 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
+main () {
+    [[ "$1" == "encode" ]] && en "$2"
+    [[ "$1" == "decode" ]] && de "$2"
+    exit 0
+}
+
+en () {
+    # put each character in a new line
+    grep -o '.' <<< "$1" |
+        # count the runs
+        uniq -c |
+        # remove the space in between
+        awk '{print $1$2}' |
+        # replace newlines with spaces
+        tr -s '\n' ' ' |
+        # remove the special case of a single space in between letters
+        # remove all 1s but not 11s in front of letters
+        # remove leftover spaces
+        sed -re 's/ 1 /  /g' \
+        -re 's/([[:space:]]*)1([[:alpha:]])/\1\2/g' \
+        -re 's/([[:alpha:]])[[:space:]]/\1/g'
+}
+
+# decode solution stolen from glennj
+de () {
+    local phrase=$1
+
+    while [[ $phrase =~ ([[:digit:]]+)([^[:digit:]]) ]]; do
+        printf -v phrase "%s%s%s" \
+            "${phrase%%${BASH_REMATCH[0]}*}" \
+            "$(str::repeat "${BASH_REMATCH[2]}" "${BASH_REMATCH[1]}")" \
+            "${phrase#*${BASH_REMATCH[0]}}"
+    done
+    echo "$phrase"
+}
+
+# Repeat a character a specified number of times
 #
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
+# parameters
+# - character
+# - count
 #
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
+# usage: strRepeat "*" 60
 #
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+str::repeat() {
+    local char=$1 count=$2
+    local result
+    # string of count spaces
+    printf -v result "%*s" "$count" ""
+    # replace spaces with the char
+    echo "${result// /$char}"
+}
+
+main "$@"
