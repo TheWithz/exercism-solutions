721e26c1e7d52e930944417de8f926e1b2735477
thewithz <8269545+thewithz@users.noreply.github.com>
2021-04-18 21:43:22 -0400

    Prefer not to use temporary files

--- download_track.sh
+++ download_track.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 main() {
-    (( "$#" != 1 )) && usage && exit
-    TRACK="$1"
-    FILE="$(mktemp)"
-    curl -LS https://exercism.io/tracks/$TRACK/exercises | grep "/tracks/$TRACK/exercises/" | awk '{print $3}' | cut -d/ -f5 | cut -d\" -f1 > $FILE
-    for word in `sed '/^$/d' $FILE`; do exercism download --exercise="$word" --track=$TRACK; done
+  (( $# == 1 )) || usage
+  TRACK="$1"
+  curl -LSs "https://exercism.io/tracks/$TRACK/exercises" |
+    grep "/tracks/$TRACK/exercises/" |
+    awk '{print $3}' |
+    cut -d'/' -f'5' |
+    cut -d'"' -f'1' |
+    xargs -I{} sh -c "exercism download --exercise={} --track=$TRACK || echo '{}'"
 }
 
 usage() {
-   echo "Usage: exercism-download.sh <Track>" 
+  echo "Usage: exercism-download.sh <Track>" 
+  exit 1
 }
 
 main "$@"
